d7a0458249a0afb7e9181f40ab7131e9ae6415fc
Alejandro <jacxdev@gmail.com>
2020-04-21 01:48:50 -0500

    Se agregan las funciones -> agregar tarea, tarea terminada, eliminar tarea

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const app = new Vue({
     el: '#app',
     data: {
-        titulo: 'CRUD Vue.js'
+        titulo: 'Vue Task',
+        tareas: [],
+        nuevaTarea: ''
+    },
+    methods:{
+        agregarTarea: function(){
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            })
+            this.nuevaTarea = '';
+        },
+        editarTarea: function(index){
+            this.tareas[index].estado = true;
+        },
+        eliminar: function(index){
+            this.tareas.splice(index, 1)
+        }
     }
 })
 
